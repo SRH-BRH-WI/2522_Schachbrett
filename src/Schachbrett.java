4a133e13bd2f8ef671d9c2d6b667d08aa090c2f0
Dr. Dillinger <peter.dillinger@srh.de>
2023-01-17 15:43:24 +0100

    Zwei mögliche (sehr ähnliche) Lösungen

--- src/Schachbrett.java
+++ src/Schachbrett.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Schachbrett {
 
     // Eingabe: Größe des Schachbretts: 8
@@ -12,6 +14,33 @@ public class Schachbrett {
     //  8  9 10 11 12 13 14 15
 
     public static void main(String[] args) {
+        Scanner eingabe = new Scanner(System.in);
+        System.out.print("Größe des Schachbretts: ");
+        int größe = eingabe.nextInt();
 
+        for (int zeile=1; zeile <= größe; zeile++) {
+
+            for (int spalte =zeile; spalte < größe+zeile; spalte++) {
+                if (spalte < 10) System.out.print(" ");
+                System.out.print(" " + spalte);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main2(String[] args) {
+        Scanner eingabe = new Scanner(System.in);
+        System.out.print("Größe des Schachbretts: ");
+        int größe = eingabe.nextInt();
+
+        for (int zeile = 1; zeile <= größe; zeile++) {
+            for (int spalte = 0; spalte < größe; spalte++) {
+                int wert = zeile + spalte;
+                if (wert < 10) System.out.print(" ");
+                System.out.print(" " + wert);
+            }
+            System.out.println();
+        }
     }
 }
